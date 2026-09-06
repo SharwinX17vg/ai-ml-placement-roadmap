d6f693bb098f552b8550bfc07810d96837af5349
SharwinX17vg <vgnithiyasharwin@gmail.com>
2026-09-06 21:14:55 +0530

    Practice Python if, elif and else decision statements

---
+++ 01-python/06_if_else.py
@@ -0,0 +1,23 @@
+marks = 82
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 50:
+    print("Grade C")
+else:
+    print("Fail")
+
+
+ #Python stops after the first True condition.
+age = 20
+has_id = True
+
+if age >= 18:
+    if has_id:
+        print("Entry Allowed")
+    else:
+        print("ID Required")
+else:
+    print("Under Age")
