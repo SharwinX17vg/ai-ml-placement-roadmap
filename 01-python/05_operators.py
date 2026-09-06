73273612d4f335ca1b6a74c86dbbbdd67d540e80
SharwinX17vg <vgnithiyasharwin@gmail.com>
2026-09-06 20:55:55 +0530

    Learn Python arithmetic, comparison and logical operators

---
+++ 01-python/05_operators.py
@@ -0,0 +1,50 @@
+x = 20
+y = 6
+print("Addition       :", x + y)
+print("Subtraction    :", x - y)
+print("Multiplication :", x * y)
+print("Division       :", x / y)
+print("Floor Division :", x // y)
+print("Modulus        :", x % y)
+print("Power          :", x ** y)
+print("Equal             :", x == y)
+print("Not Equal         :", x != y)
+print("Greater Than      :", x > y)
+print("Less Than         :", x < y)
+print("Greater or Equal  :", x >= y)
+print("Less or Equal     :", x <= y)
+age = 20
+has_id = True
+print(age >= 18 and has_id)
+print(age >= 18 or has_id)
+print(not has_id)
+score = 10
+score += 5
+print(score)
+score -= 2
+print(score)
+score *= 3
+print(score)
+score /= 2
+print(score)
+mark1 = 85
+mark2 = 90
+mark3 = 95
+average = (mark1 + mark2 + mark3) / 3
+print(f"Average Marks = {average}")
+weight = 65
+height = 1.7
+bmi = weight /(height ** 2)
+print(bmi)
+weight = 65
+height = 1.7
+bmi = weight / (height ** 2)
+print(bmi)
+weight = 65
+height = 1.7
+bmi = weight / (height ** 2)
+print(bmi)
+correct_predictions = 92
+total_predictions = 100
+accuracy = (correct_predictions / total_predictions) * 100
+print(f"Accuracy = {accuracy}%")
